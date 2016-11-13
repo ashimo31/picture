5f0497b4750c6769577aae1bdfed6abc3f19a5a4
ashimo31 <nobuom@MoriNobuos-MacBook-Air.local>
2016-11-13 13:00:27 +0900

    dive14e

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,33 @@
 class TopController < ApplicationController
-  def index
+
+
+def index
+  @photos = Photo.all
+  #raise
+end
+
+def new
+  if params[:back]
+   @photo = Photo.new(photos_params)
+ else
+  @photo = Photo.new
+ end
+end
+
+def confirm
+  @photo = Photo.new(photos_params)
+  render :new if @photo.invalid?
+end
+
+def create
+     @photo=Photo.new(photos_params)
+     @photo.user_id = current_user.id
+  if @photo.save
+    redirect_to photos_path, notice: "投稿しました！"
+    NoticeMailer.sendmail_photo(@photo).deliver
+  else
+    render action: 'new'
   end
+end
+
 end
